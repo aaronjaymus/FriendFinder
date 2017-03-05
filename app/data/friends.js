bcf88f7799780f86fedb6b55d96468270dbefdad
aaronjaymus <aaronjaymus@gmail.com>
2017-03-05 16:29:50 -0500

    Website is built. Runs off server. Sending new friends to API works. Form validation doesn't seem to work and won't clear the fields when submitted.

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+	{
+		name: "James McHale",
+		photo: "http://i.imgur.com/pmyIdkB.png",
+		scores: [
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3,
+			3
+		]
+	}
+];
+
+module.exports = friendsArray;
