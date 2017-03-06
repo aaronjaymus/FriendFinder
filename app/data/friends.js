8483535420c17f0c71c774be7cc29f64436f0f06
aaronjaymus <aaronjaymus@gmail.com>
2017-03-06 16:27:46 -0500

    Now when sending a survey response, it takes your POST request and sends back your closest match.

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,31 @@ var friendsArray = [
 		name: "James McHale",
 		photo: "http://i.imgur.com/pmyIdkB.png",
 		scores: [
-			3,
-			3,
-			3,
-			3,
-			3,
-			3,
-			3,
-			3,
-			3,
-			3
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3",
+			"3"
+		]
+	},{
+		name: "Kimberly McHale",
+		photo: "http://i.imgur.com/XWthQuw.jpg",
+		scores: [
+			"1",
+			"2",
+			"3",
+			"4",
+			"5",
+			"4",
+			"3",
+			"2",
+			"1",
+			"1"
 		]
 	}
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,24 @@ module.exports = function(app) {
 	});
 
 	app.post("/api/friends", function(req, res){
+		var newFriend=req.body;
+		var friendFound;
+		var friendsValue=null;
+
+		for(var i=0; i<friendsData.length; i++){
+			var currentValue=0;
+			for(var j=0; j<friendsData[i].scores.length; j++){
+				currentValue+=Math.abs(friendsData[i].scores[j]-newFriend.scores[j]);
+				console.log(friendsData[i].name+" "+currentValue);
+			}
+			if(currentValue<friendsValue || friendsValue===null){
+				friendsValue = currentValue;
+				friendFound = friendsData[i];
+			}
+			console.log(JSON.stringify(friendFound));	
+		}
+		res.json(friendFound);
 		friendsData.push(req.body);
+
 	});
 };
